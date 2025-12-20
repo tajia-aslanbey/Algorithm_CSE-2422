3638f09e9ff2855e5fd4f57cfab84b31b3f5ce98
tajia-aslanbey <tajiajahan5@gmail.com>
2025-12-20 06:25:54 -0800

    Update Huffman Coding (variable length).cpp

--- Huffman Coding (variable length).cpp
+++ Huffman Coding (variable length).cpp
@@ -1,54 +1,59 @@
-#include <iostream>
-#include <queue>
-#include <map>
+#include<bits/stdc++.h>
 using namespace std;
 
-struct Node {
-    char ch;
-    int freq;
-    Node *left, *right;
-    Node(char c, int f) : ch(c), freq(f), left(nullptr), right(nullptr) {}
-};
+int main(){
+    //BCCABBDDAECCBBAEDDCC
+    string text;
+    cin>>text;
 
-struct Compare {
-    bool operator()(Node* a, Node* b) {
-        return a->freq > b->freq;
-    }
-};
+    map<char,int>freq;
+    for(char c:text)freq[c]++;
 
-void printCodes(Node* root, string str, map<char, string>& huffmanCode) {
-    if (!root) return;
-    if (!root->left && !root->right) {
-        huffmanCode[root->ch] = str;
-    }
-    printCodes(root->left, str + "0", huffmanCode);
-    printCodes(root->right, str + "1", huffmanCode);
-}
-
-int main() {
-    map<char, int> freq = {{'a', 5}, {'b', 9}, {'c', 12}, {'d', 13}, {'e', 16}, {'f', 45}};
-    priority_queue<Node*, vector<Node*>, Compare> pq;
-
-    for (auto& p : freq) {
-        pq.push(new Node(p.first, p.second));
+    cout<<"Frequencies : "<<endl;
+    for(auto it : freq) {
+        cout<<it.first<<" : "<<it.second<<endl;
     }
 
-    while (pq.size() != 1) {
-        Node* left = pq.top(); pq.pop();
-        Node* right = pq.top(); pq.pop();
-        Node* newNode = new Node('\0', left->freq + right->freq);
-        newNode->left = left;
-        newNode->right = right;
-        pq.push(newNode);
+    cout<<endl;
+
+    priority_queue<pair<int,string>,vector<pair<int,string>>,greater<pair<int,string>>>pq;
+
+    for(auto it:freq){
+        pq.push({it.second,string(1,it.first)});
     }
 
-    map<char, string> huffmanCode;
-    printCodes(pq.top(), "", huffmanCode);
+    map<char,string>code;
 
-    cout << "Char\tFreq\tCode\n";
-    for (auto& p : freq) {
-        cout << p.first << "\t" << p.second << "\t" << huffmanCode[p.first] << "\n";
+    while(pq.size()>1){
+        auto left = pq.top();pq.pop();
+        auto right = pq.top(); pq.pop();
+
+        for(char c:left.second)code[c]="0"+code[c];
+        for(char c:right.second)code[c]="1"+code[c];
+
+        pq.push({left.first+right.first,left.second+right.second});
     }
+
+    cout<<"Huffman Codes : "<<endl;
+
+    for(auto it:freq) {
+        cout<<it.first<<" : "<<code[it.first]<<endl;
+    }
+    cout<<endl;
+
+    int original= text.length()*8;
+    int compressed = 0;
+    int table_bits = 0;
+
+    for(auto it:freq) {
+        compressed+=it.second*code[it.first].length();
+        table_bits+=8+code[it.first].length();
+    }
+
+    cout <<"Original Length : "<< original << endl;
+    cout<<"Compressed message length : "<<compressed<<endl;
+    cout<<"Table : "<<table_bits<<endl;
+    cout <<"New length : "<<compressed+table_bits << endl;
 
     return 0;
 }
